e0290e869bd3d7e37707ad202b7394644d00abe7
Spencer Rose <spencer.rose@gov.bc.ca>
2025-08-11 17:09:17 -0700

    Include SMTP settings for mail notifications.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -84,6 +84,15 @@ ini_set('display_errors', 1);
 \$wgDevelopmentWarnings = false;
 \$wgShowDBErrorBacktrace = true;
 
+# # --- MediaWiki SMTP Settings ---
+\$wgSMTP = [
+    'host'      => "$MEDIAWIKI_SMTP_HOST",
+    'IDHost'    => "$MEDIAWIKI_SMTP_ID_HOST",
+    'localhost' => "$MEDIAWIKI_SMTP_LOCALHOST",
+    'port'      => "$MEDIAWIKI_SMTP_PORT",
+    'auth'      => "$MEDIAWIKI_SMTP_AUTH"
+];
+
 # # --- Custom Extensions ---
 # # Load VisualEditor and its dependencies
 # wfLoadExtension( 'VisualEditor' );
